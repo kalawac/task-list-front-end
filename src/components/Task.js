ffe96bcb98b3ae90da1cb4f6bfcbb8a4b7d0e7cb
Jennifer <datateur@gmail.com>
2022-12-16 11:45:54 -0800

    Wave 2 incomplete

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,19 +3,33 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isCompleteData }) => {
-  const buttonClass = isCompleteData ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+  const onCompleteButtonClick = () => {
+    const updatedTask = {
+      id: props.id,
+      title: props.title,
+      isCompleteData: !props.isCompleteData,
+    };
+    props.onUpdate(updatedTask);
+  };
+
+  const buttonClass = props.isCompleteData ? 'tasks__item__toggle--completed' : '';
 
   return (
-    <li className="tasks__item">
-      <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => isCompleteData } // placeholder
-      >
-        {title}
-      </button>
-      <button className="tasks__item__remove button">x</button>
-    </li>
+    <div>
+      <li className="tasks__item">
+        <button
+          className={`tasks__item__toggle ${buttonClass}`}
+          onClick={{ onCompleteButtonClick }}
+        >
+          {props.title}
+        </button>
+        <button className="tasks__item__remove button">x</button>
+      </li>
+      <li>
+        Is the data complete???? {props.isCompleteData}
+      </li>
+    </div>
   );
 };
 
