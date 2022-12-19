af2cc979fe3a36a2cd01e051e091997cb71465b7
Katherine Abrikian <kabrikian@gmail.com>
2022-12-18 23:40:39 -0500

    updated TASKS key from isComplete to isCompleteData, in line with rest of code

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ const TASKS = [
   {
     id: 1,
     title: 'Mow the lawn',
-    isComplete: false,
+    isCompleteData: false,
   },
   {
     id: 2,
     title: 'Cook Pasta',
-    isComplete: true,
+    isCompleteData: true,
   },
 ];
 
